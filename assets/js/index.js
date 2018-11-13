e18f508b97c78827365551692b5c29d2fe5c28e8
Charles Jin <cjin32@gmail.com>
2018-11-12 22:55:13 -0600

    professional personal webpage

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+var clickable = document.getElementsByClassName("clickable");
+var numClickable = clickable.length;
+
+for (var i = 0; i < numClickable; i++) {
+  clickable[i].parentNode.querySelector("div").style.display = "none";
+
+  clickable[i].addEventListener("mouseenter", function(event) {   
+    event.target.style.textDecoration = "underline";
+    event.target.style.cursor = "pointer";
+  });
+
+  clickable[i].addEventListener("mouseout", function(event) {   
+    event.target.style.textDecoration = "";
+    event.target.style.cursor = "";
+  });
+
+  clickable[i].addEventListener("click", function(event) {
+    var displayDiv = event.target.parentNode.querySelector("div");
+    if (!displayDiv.style.display) {
+      displayDiv.style.display = "none";
+    } else {
+      displayDiv.style.display = "";
+    }
+  });
+};
